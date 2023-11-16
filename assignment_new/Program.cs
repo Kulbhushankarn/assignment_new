a2d97bb7edcbfbd93fc59012f27e0550d8a750dc
Kulbhushan <karnkulbhushan@gmail.com>
2023-11-16 21:30:27 +0530

    Add project files.

---
+++ assignment_new/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Example values
+        int a = 10;
+        int b = 2;
+
+        int result = a * (b / 2);
+
+        
+        Console.WriteLine("Result: " + result);
+    }
+}
